19d730b2d3e3071e56d82301ae8716ee1d8332c0
Kornkawin Bijayayothin <outsource1456@setth.onmicrosoft.com>
2024-04-21 21:44:17 +0700

    [RASPI-UNO] update analytics

--- mqtt/raspi_uno/temperature_analytics.py
+++ mqtt/raspi_uno/temperature_analytics.py
@@ -33,22 +33,27 @@ if __name__ == '__main__':
     if sim_data.empty:
         print('No simulation data available')
     else:
-        print(sim_data['temperature'].describe(), '\n')
-        print(sim_data, '\n')
-        # Plot the simulation data
         # get latest 10 minutes data
         sim_data = sim_data.sort_values('timestamp')
         sim_data = sim_data[sim_data['timestamp'] > sim_data['timestamp'].max() - pandas.Timedelta(minutes=TIME_INTERVAL)]
+        print(sim_data['temperature'].describe(), '\n')
+        print(sim_data, '\n')
+
+        # Plot the simulation data
+        sim_data['avg'] = sim_data['temperature'].mean()
         sim_data.set_index('timestamp', inplace=True, drop=True)
         fig, ax = plt.subplots(figsize=(7, 7))
         fig.autofmt_xdate()
-        plt.title('Simulated Temperature Data')
+        plt.title(f'Simulated Temperature Data (Latest {TIME_INTERVAL} Minutes)')
         plt.ylabel('Temperature')
         plt.xticks(rotation=45)
         xfmt = md.DateFormatter('%m-%d %H:%M:%S')
         ax.xaxis.set_major_formatter(xfmt)
         plt.plot('temperature', data=sim_data, label='temperature_sim')
-        plt.show()
+        plt.plot('avg', data=sim_data, label='average', linestyle='--', color='red')
+        # plt.show()
+        plt.savefig('sim.png')
+        plt.close()
 
     # Display description of real data
     print('===========================================================')
@@ -58,20 +63,24 @@ if __name__ == '__main__':
     if real_data.empty:
         print('No real data available')
     else:
-        print(real_data['temperature'].describe(), '\n')
-        print(real_data, '\n')
-        plt.plot(real_data['temperature'])
-        # Plot the real data
         # get latest 10 minutes data
         real_data = real_data.sort_values('timestamp')
         real_data = real_data[real_data['timestamp'] > real_data['timestamp'].max() - pandas.Timedelta(minutes=TIME_INTERVAL)]
+        print(real_data['temperature'].describe(), '\n')
+        print(real_data, '\n')
+
+        # Plot the real data
+        real_data['avg'] = real_data['temperature'].mean()
         real_data.set_index('timestamp', inplace=True, drop=True)
         fig, ax = plt.subplots(figsize=(7, 7))
         fig.autofmt_xdate()
-        plt.title('Real Temperature Data')
+        plt.title(f'Real Temperature Data (Latest {TIME_INTERVAL} Minutes)')
         plt.ylabel('Temperature')
         plt.xticks(rotation=45)
         xfmt = md.DateFormatter('%m-%d %H:%M:%S')
         ax.xaxis.set_major_formatter(xfmt)
         plt.plot('temperature', data=real_data, label='temperature_real')
-        plt.show()
+        plt.plot('avg', data=real_data, label='average', linestyle='--', color='red')
+        # plt.show()
+        plt.savefig('real.png')
+        plt.close()
